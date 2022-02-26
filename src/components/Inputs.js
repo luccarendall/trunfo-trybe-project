451c7b7f566c3c2fb99e69a3f53a8618b792b7e6
Lucca Rendall <luccarendall1@gmail.com>
2022-02-26 00:10:44 -0300

    Correçao EsLint - Labels

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -10,33 +10,49 @@ class Inputs extends React.Component {
 
     return (
       <>
-        <label htmlFor="name">Nome:</label>
-        <input type="text" id="name" name="name" data-testid="name-input" value={ cardName } onChange={ onInputChange } />
+        <label htmlFor="name">
+          Nome:
+          <input type="text" id="name" name="name" data-testid="name-input" value={ cardName } onChange={ onInputChange } />
+        </label>
 
-        <label htmlFor="bio">Descrição:</label>
-        <input type="textarea" id="bio" name="bio" data-testid="description-input" value={ cardDescription } onChange={ onInputChange } />
+        <label htmlFor="bio">
+          Descrição:
+          <input type="textarea" id="bio" name="bio" data-testid="description-input" value={ cardDescription } onChange={ onInputChange } />
+        </label>
 
-        <label htmlFor="attr1">Attr1:</label>
-        <input type="number" id="attr1" name="attr1" data-testid="attr1-input" value={ cardAttr1 } onChange={ onInputChange } />
+        <label htmlFor="attr1">
+          Attr1:
+          <input type="number" id="attr1" name="attr1" data-testid="attr1-input" value={ cardAttr1 } onChange={ onInputChange } />
+        </label>
 
-        <label htmlFor="attr2">Attr2:</label>
-        <input type="number" id="attr2" name="attr2" data-testid="attr2-input" value={ cardAttr2 } onChange={ onInputChange } />
+        <label htmlFor="attr2">
+          Attr2:
+          <input type="number" id="attr2" name="attr2" data-testid="attr2-input" value={ cardAttr2 } onChange={ onInputChange } />
+        </label>
 
-        <label htmlFor="attr3">Attr3:</label>
-        <input type="number" id="attr3" name="attr3" data-testid="attr3-input" value={ cardAttr3 } onChange={ onInputChange } />
+        <label htmlFor="attr3">
+          Attr3:
+          <input type="number" id="attr3" name="attr3" data-testid="attr3-input" value={ cardAttr3 } onChange={ onInputChange } />
+        </label>
 
-        <label htmlFor="img">Imagem:</label>
-        <input type="text" id="img" name="img" data-testid="image-input" value={ cardImage } onChange={ onInputChange } />
+        <label htmlFor="img">
+          Imagem:
+          <input type="text" id="img" name="img" data-testid="image-input" value={ cardImage } onChange={ onInputChange } />
+        </label>
 
-        <label htmlFor="rare" value={ cardRare } onChange={ onInputChange }>Raridade:</label>
-        <select id="rare" name="rare" data-testid="rare-input">
-          <option value="normal">Normal</option>
-          <option value="raro">Raro</option>
-          <option value="muito raro">Muito raro</option>
-        </select>
+        <label htmlFor="rare" value={ cardRare } onChange={ onInputChange }>
+          Raridade:
+          <select id="rare" name="rare" data-testid="rare-input">
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muito raro">Muito raro</option>
+          </select>
+        </label>
 
-        <label htmlFor="checkboxInput">CheckBox:</label>
-        <input type="checkbox" id="checkbox" name="checkbox" data-testid="trunfo-input" value={ cardTrunfo } onChange={ onInputChange } />
+        <label htmlFor="checkboxInput">
+          CheckBox:
+          <input type="checkbox" id="checkbox" name="checkbox" data-testid="trunfo-input" value={ cardTrunfo } onChange={ onInputChange } />
+        </label>
 
         {/* onClick={ save }> */}
         <button type="submit" data-testid="save-button" disabled={ isSaveButtonDisabled } onClick={ onSaveButtonClick }>Salvar</button>
