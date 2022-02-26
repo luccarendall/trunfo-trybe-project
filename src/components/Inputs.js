5d316dd71173d0827f840efc1896e0a042694808
Lucca Rendall <luccarendall1@gmail.com>
2022-02-26 06:06:06 -0300

    Corrigindo o ESLint agora vai

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,42 +1,87 @@
-/* eslint-disable max-len */
 import React from 'react';
 import PropType from 'prop-types';
 
 class Inputs extends React.Component {
   render() {
     // Adicionar prop "hasTrunfo" abaixo
-    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage, cardRare, cardTrunfo, isSaveButtonDisabled, onInputChange, onSaveButtonClick } = this.props;
+    const {
+      cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage, cardRare,
+      cardTrunfo, isSaveButtonDisabled, onInputChange,
+      onSaveButtonClick } = this.props;
 
     return (
       <>
         <label htmlFor="name">
           Nome:
-          <input type="text" id="name" name="name" data-testid="name-input" value={ cardName } onChange={ onInputChange } />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            data-testid="name-input"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="bio">
           Descrição:
-          <input type="textarea" id="bio" name="bio" data-testid="description-input" value={ cardDescription } onChange={ onInputChange } />
+          <input
+            type="textarea"
+            id="bio"
+            name="bio"
+            data-testid="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="attr1">
           Attr1:
-          <input type="number" id="attr1" name="attr1" data-testid="attr1-input" value={ cardAttr1 } onChange={ onInputChange } />
+          <input
+            type="number"
+            id="attr1"
+            name="attr1"
+            data-testid="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="attr2">
           Attr2:
-          <input type="number" id="attr2" name="attr2" data-testid="attr2-input" value={ cardAttr2 } onChange={ onInputChange } />
+          <input
+            type="number"
+            id="attr2"
+            name="attr2"
+            data-testid="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="attr3">
           Attr3:
-          <input type="number" id="attr3" name="attr3" data-testid="attr3-input" value={ cardAttr3 } onChange={ onInputChange } />
+          <input
+            type="number"
+            id="attr3"
+            name="attr3"
+            data-testid="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="img">
           Imagem:
-          <input type="text" id="img" name="img" data-testid="image-input" value={ cardImage } onChange={ onInputChange } />
+          <input
+            type="text"
+            id="img"
+            name="img"
+            data-testid="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="rare" value={ cardRare } onChange={ onInputChange }>
@@ -48,13 +93,28 @@ class Inputs extends React.Component {
           </select>
         </label>
 
-        <label htmlFor="checkbox  ">
-          <input type="checkbox" id="checkbox" name="checkbox" data-testid="trunfo-input" value={ cardTrunfo } onChange={ onInputChange } />
+        <label htmlFor="checkbox">
+          <input
+            type="checkbox"
+            id="checkbox"
+            name="checkbox"
+            data-testid="trunfo-input"
+            value={ cardTrunfo }
+            onChange={ onInputChange }
+          />
           Super Trybe Trunfo
         </label>
         <br />
         {/* onClick={ save }> */}
-        <button type="submit" data-testid="save-button" disabled={ isSaveButtonDisabled } onClick={ onSaveButtonClick }>Salvar</button>
+        <button
+          type="submit"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+
+        </button>
       </>
     );
   }
