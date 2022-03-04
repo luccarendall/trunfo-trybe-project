ffd6a3e228c7a975faa1fc413ffdbfb9cd67a78a
Lucca Rendall <luccarendall1@gmail.com>
2022-03-04 09:19:41 -0300

    Requisito 5 - Validaçao do botão de Salvar

--- src/App.js
+++ src/App.js
@@ -13,15 +13,54 @@ class App extends React.Component {
       cardAttr2: '',
       cardAttr3: '',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: '',
+      isSaveButtonDisabled: true,
     };
     this.onInputChange = this.onInputChange.bind(this);
+    this.saveButtonDisabled = this.saveButtonDisabled.bind(this);
   }
 
   onInputChange({ target }) {
     const info = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({ [target.name]: info });
+    this.setState({ [target.name]: info }, this.saveButtonDisabled);
+  }
+
+  // Referência para o saveButtonDisabled: https://github.com/tryber/sd-019-c-project-tryunfo/pull/88/
+  saveButtonDisabled() {
+    const min = 0;
+    const max = 90;
+    const maxsum = 210;
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+    const sum = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+
+    if (
+      cardName !== ''
+      && cardDescription !== ''
+      && cardAttr1 >= min
+      && cardAttr1 <= max
+      && cardAttr2 >= min
+      && cardAttr2 <= max
+      && cardAttr3 >= min
+      && cardAttr3 <= max
+      && cardImage !== ''
+      && cardRare !== ''
+      && sum <= maxsum
+      && cardRare.length <= maxsum
+    ) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   }
 
   render() {
